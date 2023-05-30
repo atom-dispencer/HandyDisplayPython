a0d48c9928073eaa47d9675fb852d440375abff9
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-05-30 20:28:05 +0100

    TODO: Change Weather Timeout

--- handy_display/widgets/WeatherWidget.py
+++ handy_display/widgets/WeatherWidget.py
@@ -9,7 +9,7 @@ from pygame import Surface
 from handy_display import Secrets
 from handy_display.widgets.IWidget import IWidget
 
-TIMEOUT_NS = 5_000_000_000  # 60 second timeout
+TIMEOUT_NS = 5_000_000_000  # 5 second timeout #TODO Change weather timeout to >5s (revert from testing)
 THREAD_NAME = "weather_refresh_thread"
 
 
