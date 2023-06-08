4a1cf8a011334f5b15fc3a9bd54264049deada43
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-06-08 15:56:37 +0100

    Update ImageHelper.py

--- handy_display/ImageHelper.py
+++ handy_display/ImageHelper.py
@@ -4,6 +4,9 @@ search_dir = "resources/images/"
 
 
 def get_scaled(path: str, w: int, h: int) -> pygame.Surface:
+    if not pygame.get_init():
+        raise Exception("Pygame must be initialised before using ImageHelper ")
+        
     img = pygame.image.load(search_dir + path)
     surf = pygame.Surface((w, h), pygame.SRCALPHA)
     return pygame.transform.scale(img, (w, h), surf)
