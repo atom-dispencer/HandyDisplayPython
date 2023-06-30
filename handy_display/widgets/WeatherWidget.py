ed0cd4dddc7283fdc54fcf44c59dbc6f5d2d59ac
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-06-30 20:21:00 +0100

    Text for today's data
    > Darker orange boxes on weather widget are now drawn dynamically, not painted onto weather.bmp (makes it a bit easier to align their text and change it later)
    + Placeholder text now displayed for today's data

--- handy_display/widgets/WeatherWidget.py
+++ handy_display/widgets/WeatherWidget.py
@@ -3,6 +3,7 @@ import json
 import threading
 import time
 
+import numpy
 import requests
 
 from handy_display import Secrets
@@ -19,9 +20,31 @@ JORDY_BLUE = (158, 183, 229)
 CORNFLOWER_BLUE = (100, 141, 229)
 YInMn_BLUE = (48, 76, 137)
 
+DARK_SAGE = (108, 106, 96)
+
 BACKGROUND = smooth_image("weather/weather.bmp", 480, 320)
+TODAY_BOUNDS = (50, 40, 190, 260)
+TODAY_DATA = smooth_image("weather/today_data.bmp", 161, 48)
+# Bounds of data sections relative to the bounds of TODAY_DATA
+# Start at (65, 67) and transform by (0, -53) for each
+YESTERDAY_BOUNDS = (260, 50, 170, 130)
+VISIBILITY_BOUNDS = (260, 190, 170, 100)
 
 ROBOTO_16 = font("Roboto/Roboto-Black.ttf", 16)
+ROBOTO_20 = font("Roboto/Roboto-Black.ttf", 20)
+
+
+def draw_one_today(surf: pygame.surface.Surface, pos: tuple[float, float], temp: float, wind: float):
+    temp_txt = "{temp}K".format(temp=round(temp, 2))
+    wind_txt = "{wind}m/s".format(wind=round(wind, 1))
+    temp_render = ROBOTO_20.render(temp_txt, True, DARK_SAGE)
+    wind_render = ROBOTO_20.render(wind_txt, True, DARK_SAGE)
+    temp_pos = numpy.add(pos, (5, 0))
+    wind_pos = numpy.add(pos, (5, 24))
+
+    surf.blit(TODAY_DATA, pos)
+    surf.blit(temp_render, temp_pos)
+    surf.blit(wind_render, wind_pos)
 
 
 class WeatherWidget(IWidget):
@@ -34,15 +57,25 @@ class WeatherWidget(IWidget):
         # "units": "standard",  # standard=K, imperial=F, metric=C, K is default
         # "mode": "xml",  # JSON is default
         # "lang": "en",  # Response language
-        "today": [],
-        "yesterday": []
+        "today": [
+            {"temp": -273.15, "wind": -1},
+            {"temp": -273.15, "wind": -1},
+            {"temp": -273.15, "wind": -1},
+            {"temp": -273.15, "wind": -1},
+        ],
+        "yesterday": [
+            {"temp": -273.15},
+            {"temp": -273.15},
+            {"temp": -273.15},
+            {"temp": -273.15},
+        ]
     }
 
     def __init__(self, gui):
         super().__init__(gui, self.INTERNAL_NAME, self.DISPLAY_NAME, self.DEFAULT_CONFIG)
         self.last_refresh_ns = 0
-        self.refresh_thread = None
-        self.open_weather_data = None
+        self.refresh_thread: threading.Thread = None
+        self.open_weather_data: dict = None
         self.day_frac_elapsed = 0
 
     def on_show(self):
@@ -72,7 +105,15 @@ class WeatherWidget(IWidget):
     def draw(self, surf: pygame.surface.Surface):
         surf.blit(BACKGROUND, (0, 0))
 
+        pos = (65, 67)
+        for i in range(4):
+            temp = self.config["today"][i]["temp"]
+            wind = self.config["today"][i]["wind"]
+            draw_one_today(surf, pos, temp, wind)
+            pos = numpy.add(pos, (0, 53))
+
     def on_hide(self):
+        # No need to kill the refreshing thread
         pass
 
     def update_info(self):
@@ -81,8 +122,17 @@ class WeatherWidget(IWidget):
 
             key = Secrets.get_weather_api_key()
             url = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&cnt={cnt}&appid={appid}" \
-                .format(lat=self.config["lat"], lon=self.config["lon"], cnt=self.config["cnt"], appid=key)
-            response = requests.get(url)
+                .format(
+                    lat=self.config["lat"],
+                    lon=self.config["lon"],
+                    cnt=self.config["cnt"],
+                    appid=key
+                )
+            response = requests.get(url, timeout=10)
+
+            if response is None or response.status_code != 200:
+                print("Error getting new OpenWeather data!")
+                print(str(response))
             print("Got OpenWeather reply: ", response.content)
 
             json_content = json.loads(response.content)
